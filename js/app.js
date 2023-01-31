43dd1d333a55ade137508de64056cb1d567885c0
albertopiffer <albertopiffer96@gmail.com>
2023-01-31 16:21:42 +0100

    bonus senza modifiche la display

--- js/app.js
+++ js/app.js
@@ -8,12 +8,43 @@ console.log(scontoO65)
 let nKm = parseInt(prompt('Numero di km:'))
 console.log(nKm)
 
+let NomCog = prompt('Nome e Cognome:')
+console.log(NomCog)
+
 let eta = parseInt(prompt('Età passeggero:'))
 console.log(eta)
 
 let prezzo = parseFloat
 
+const km = document.getElementById('km')
+km.innerHTML = nKm
+
+const nome = document.getElementById('nome')
+nome.innerHTML = NomCog
+
 prezzo = nKm * prezzoKm
-if (eta < 19) prezzo = prezzo * (1 - scontoMin)
-if (eta > 64) prezzo = prezzo * (1 - scontoO65)
-console.log(prezzo)
+
+const prezzoBase = document.getElementById('prezzoBase')
+prezzoBase.innerHTML = prezzo
+
+if (eta < 19) {
+    prezzo = prezzo * (1 - scontoMin)
+    console.log(prezzo)
+
+    const sconto = document.getElementById('sconto')
+    sconto.innerHTML = 20
+
+    const prezzoFinale = document.getElementById('prezzoFinale')
+    prezzoFinale.innerHTML = prezzo
+}
+
+if (eta > 64) {
+    prezzo = prezzo * (1 - scontoO65)
+    console.log(prezzo)
+
+    const sconto = document.getElementById('sconto')
+    sconto.innerHTML = 40
+
+    const prezzoFinale = document.getElementById('prezzoFinale')
+    prezzoFinale.innerHTML = prezzo
+}
